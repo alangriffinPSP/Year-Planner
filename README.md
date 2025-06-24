8f9851ba792aa3054d9b87d46b260ce9b1fa1d31
Alan Griffin <alan.griffin@petsitterplus.com>
2025-06-24 21:18:46 +0100

    Basic functionality achieved.

--- README.md
+++ README.md
@@ -1,15 +1,16 @@
 ### What it does:
     - Generates calendar based on user input (Defaults to today's date). This accurately accounts for leap years.
-    - Allows user created categories.
+    - Allows user created categories with colour coding, whilst checking for duplication.
         - Categories saved to localStorage and loaded in when planner is generated.
-    - Categories can be assigned/removed from cells
-        - Cells saved to localStorage and loaded in when corresponding dropdown selection is made.
+    - Categories can be assigned/removed from cells.
+        - Cells saved to localStorage and loaded in when planner is generated.
+        - Cells then can be filtered with dropdown box.
+        - Cells can be removed by selecting corresponding category and selecting populated cell.
     - Clear all function resets categories and cells, clears localStorage.
 
-### What it doesn't do (yet):
-    - Colour coding of categories not implemented.
+### Known issues:
+    - Poor formatting of cells with long string content.
+    - Readability conflict for certain colour choices.
 
-### Known bugs:
-    - Poor formatting of cells with content.
-    - All cell content loads regardless of dropdown option selected.
-    - If cell contains multiple entries, selecting new dropdown option shows previously removed data.
+### Possible improvements:
+    - Inefficient data structure and usage of storage.
--- yearPlanner.js
+++ yearPlanner.js
@@ -1,18 +1,3 @@
-// --- KNOWN BUGS ---
-//All cells load at the same time, not on corresponding selection
-//Colours can be defined multiple times across different categories
-//If cell contains multiple entries, changing dropdown selection shows previously removed data
-
-
-// --- TO DO ---
-
-//Change how data is stored in cells. DON'T save entire HTML string to storage
-//Implement colour indicator dot
-//Allow multiple different entries per day
-//Show/hide categories depending on dropdown selection
-//Tidy code
-
-
 $(document).ready(function () {
 
     const initialise = {
@@ -26,8 +11,8 @@ $(document).ready(function () {
                 localStorage.clear();
                 $('#category').val('');
                 $('#dropdown option:not(:first)').remove();
-                $('td').attr('class', 'dayCell').css('background-color', 'var(--bgcolor)');
                 $('.userData').remove();
+                $('.fullTitle').remove();
                 $('#colorIndicator').hide();
             } else {
                 return;
@@ -43,55 +28,104 @@ $(document).ready(function () {
             localStorage.setItem('Categories', JSON.stringify(categories));
         },
 
+        getSavedCategories() {
+            return JSON.parse(localStorage.getItem('Categories')) || [];
+        },
+
         loadCategories() {
-            const savedCategories = JSON.parse(localStorage.getItem('Categories'));
+            const savedCategories = storageControl.getSavedCategories();
 
             if (savedCategories) {
                 const categoryPairs = savedCategories.map(function (obj) {
                     return [obj.Title, obj.Color];
                 })
+
                 for (let i = 0; i < categoryPairs.length; i++) {
                     //Loads user categories into dropdown
                     $('#dropdown').append(`<option id="${categoryPairs[i][0]}">${categoryPairs[i][0]}</option>`);
                 }
             }
-
-            return savedCategories;
         },
 
-        saveCells(cell, data, test) {
+        saveCells(cell, data, color) {
+            //Grabs existing storage item or creates one with empty array
             let cells = JSON.parse(localStorage.getItem('Cells')) || [];
-            cells.push({ Cell: cell, Data: data, TEST: test});
 
+            //Try to find this cell in that array
+            let existingCell = cells.find(function (item) {
+                return item.Cell === cell;
+            })
+
+            //If so, check if category data exists in it already
+            if (existingCell) {
+                //Checks if category already exists in this cell
+                const duplicate = existingCell.Values.some(function (value) {
+                    return value.Data === data;
+                });
+
+                //If category doesn't exist, add it
+                if (!duplicate) {
+                    existingCell.Values.push({ Data: data, Color: color });
+                }
+                //If cell doesn't exist in storage create whole new object    
+            } else {
+                cells.push({
+                    Cell: cell,
+                    Values: [{ Data: data, Color: color }]
+                });
+            }
+            //commits new storage item
             localStorage.setItem('Cells', JSON.stringify(cells));
         },
 
-        //Cells loaded on dropdown change - NOT COMPLETE - Bug fix needed
         loadCells() {
             const savedCells = JSON.parse(localStorage.getItem('Cells'));
 
             if (savedCells) {
                 savedCells.forEach(function (cellObject) {
                     let cell = $("#" + cellObject.Cell)
-                    $(cell).html(cellObject.Data);
+
+                    cellObject.Values.forEach(function (value) {
+
+                        $(cell).append(`<span class="userData">${value.Data}<br></span>`);
+                        let colorTitle = $(cell).find('.userData').last();
+                        $(colorTitle).css('background-color', value.Color);
+                    })
                 })
             }
         },
 
-        removeCells(cell) {
-            const savedCells = JSON.parse(localStorage.getItem('Cells')); //Remember, this is an array
+        removeCells(cell, data) {
+            const savedCells = JSON.parse(localStorage.getItem('Cells')); //Array
+            //If no cells in storage, do nothing
+            if (!savedCells) {
+                return;
+            };
+            //Finds if a cell in storage matches that which is passed in
+            const cellObject = savedCells.find(function (item) {
+                return item.Cell === cell;
+            });
+            //If not, do nothing
+            if (!cellObject) {
+                return;
+            };
+            //Filters Values array in current cell, KEEPS those that DON'T match passed in value
+            //Effectively removing the data that matches
+            cellObject.Values = cellObject.Values.filter(function (value) {
+                return value.Data !== data;
+            });
 
-            if (savedCells) {
-                removeMe = savedCells.findIndex(indexToRemove);
-
-                function indexToRemove(item) {
+            //If Values array is now empty
+            if (cellObject.Values.length === 0) {
+                //Finds index of now empty cell object
+                let index = savedCells.findIndex(function (item) {
                     return item.Cell === cell;
-                }
-
-                savedCells.splice(removeMe, 1);
-                localStorage.setItem('Cells', JSON.stringify(savedCells));
-
+                });
+                //splices out empty cell object
+                savedCells.splice(index, 1);
             }
+            //commits updated storage item
+            localStorage.setItem('Cells', JSON.stringify(savedCells));
         }
     }
 
@@ -122,7 +156,7 @@ $(document).ready(function () {
             const months = ["January", "February", "March", "April", "May", "June",
                 "July", "August", "September", "October", "November", "December"];
 
-            //clears any previous calendar - REMOVE LATER
+            //Ensures only one calendar can be generated at a time
             $('#calendarBody').empty();
 
             //row created for each month with corresponding ids/classes
@@ -168,44 +202,55 @@ $(document).ready(function () {
 
     const cells = {
 
-        //unfinished. Sets cell content of users selection.
         setContent() {
             $('.dayCell').off('click').on('click', (function () {
-                let selectedCategory = categories.currentSelection(); //selected dropdown category
+                let selectedCategory = categories.currentSelection(); //Grabs object of selected title/colour 
+
                 let $identifier = $(this).attr('id'); //cell id to pass to storage method
-                let storedContent;
 
                 //if dropdown is default do nothing.
-                if (!selectedCategory) {
-                    return;
-                }
-                //dupe check here
+                if (!selectedCategory) { return; }
+                //check for content duplication
                 let $dupeCheck = $(this).find('.userData').filter(function () {
-                    return $(this).text() === selectedCategory;
+                    return $(this).text() === selectedCategory.title;
                 });
 
                 if ($dupeCheck.length === 0) {
-                    $(this).append(`<span class="userData">${selectedCategory}</span>`);
-                    storedContent = $(this).html(); //CHANGE THIS TO BE MORE EFFICIENT
+                    $(this).prepend(`<span class="userData">${selectedCategory.title}<br></span>`);
+                    let bg = $(this).find('.userData').first();
+                    $(bg).css('background', selectedCategory.color);
 
-                    storageControl.saveCells($identifier, storedContent, selectedCategory);
+                    storageControl.saveCells($identifier, selectedCategory.title, selectedCategory.color);
                 } else {
                     $dupeCheck.remove();
-                    storageControl.removeCells($identifier); //delete from localStorage
+                    storageControl.removeCells($identifier, selectedCategory.title); //delete from localStorage
                 }
             }))
         },
 
-        scale() {
-            $('.dayCell').hover(function () {
-                if($(this).find('span.userData').length > 0){
-                $(this).addClass('scaled');
+        display() {
+            $('.userData').each(function () {
+                let selected = categories.currentSelection();
+
+                if ($(this).text() == selected.title) {
+                    $(this).show();
+                } else {
+                    $(this).hide();
                 }
-            },
-                function () {
-                    $(this).removeClass('scaled');
-                })
-        }
+            })
+        },
+
+        // NOT IMPLEMENTED
+        // scale() {
+        //     $('.dayCell').hover(function () {
+        //         if ($(this).find('span.userData').length > 0) {
+        //             $(this).addClass('scaled');
+        //         }
+        //     },
+        //         function () {
+        //             $(this).removeClass('scaled');
+        //         })
+        // }
     }
 
     const categories = {
@@ -215,30 +260,52 @@ $(document).ready(function () {
             let $categoryColor = $('#userColor').val();
 
             //checks for duplicate entry before adding to dropdown and committing to storage
-            if ($newCategory && !categories.duplicateEntry($newCategory)) {
+            if ($newCategory && !categories.duplicateCatEntry($newCategory, $categoryColor)) {
                 $('#dropdown').append(`<option id="${$newCategory}" value="${$newCategory}">${$newCategory}</option>`);
                 //parses category to method for localStorage
                 storageControl.saveCategories($newCategory, $categoryColor);
                 $('#error').text('');
             } else {
-                $('#error').text('Category already exists');
+                $('#error').text('Duplicate entry, please try another');
             }
             $('#category').val('');
             $('#userColor').val('#000000');
         },
 
-        duplicateEntry(entry) {
-            let duplicate = $(`#dropdown option[value="${entry}"]`).length > 0;
-            return duplicate;
+        duplicateCatEntry(title, color) {
+            const savedCategories = storageControl.getSavedCategories();
+
+            return savedCategories.some(function (cat) {
+                return cat.Title === title || cat.Color === color;
+            });
         },
 
-        //returns selected dropdown category 
+        //returns selected dropdown category and corresponding color as OBJECT
         currentSelection() {
-            let currentSelection = $('#dropdown').find('option:selected').attr('id');
+            const categoryList = storageControl.getSavedCategories();
+            let selectedCategory = $('#dropdown').find('option:selected').attr('id');
 
-            if (currentSelection) {
-                console.log(currentSelection);
-                return currentSelection;
+            if (!selectedCategory) { return }
+
+            const category = categoryList.find(function (cat) {
+                return cat.Title === selectedCategory;
+            });
+
+            if (category) {
+                return {
+                    title: category.Title,
+                    color: category.Color
+                };
+            }
+        },
+
+        colorIndicator() {
+            let selection = categories.currentSelection();
+            if (selection) {
+                $('#colorIndicator').show();
+                $('#colorIndicator').css('color', selection.color);
+            } else {
+                $('#colorIndicator').hide();
             }
         }
     }
@@ -259,7 +326,9 @@ $(document).ready(function () {
 
         dropdownListener() {
             $('#dropdown').on('change', function () {
+                categories.colorIndicator();
                 categories.currentSelection();
+                cells.display();
             })
         },
 
@@ -269,11 +338,11 @@ $(document).ready(function () {
     }
 
     initialise.pageElements();
-    storageControl.loadCategories();
     categories.currentSelection();
     listeners.submitDateListener();
     listeners.categoriesListener();
     listeners.clearAllListener();
     listeners.dropdownListener();
+    storageControl.loadCategories();
 
 });
